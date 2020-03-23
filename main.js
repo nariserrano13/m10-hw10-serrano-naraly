826c9e46f837cca08139de344bebfa7c463c369d
Naraly Serrano <naralycserrano@ufl.edu>
2020-03-22 22:54:31 -0400

    Print to spans and add setTimeout

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 // It is always helpful to use comments in your code!
+var boiling;
+boiling =()=> "212";
+document.getElementById('boiling-temp').innerHTML=boiling();
+
+var freezing;
+freezing=()=>"32";
+document.getElementById('freezing-temp').innerHTML=freezing();
+
+var waterTemp;
+waterTemp=()=>"72";
+document.getElementById('water-temp').innerHTML=waterTemp();
+
+//setTimeout method 
+//created variables for elements  
+//wrote function inside the setTimeout method
+//used if/else because had multiple conditions each with diff element classes to alter
+var boilingFooter = document.getElementById('boiling');
+var frozenFooter = document.getElementById('frozen');
+var goodTempFooter = document.getElementById('good-temp');
+
+
+setTimeout( function(){
+    if(waterTemp() >= 212){
+        boilingFooter.classList.remove('hide'); 
+        boilingFooter.classList.add('show');
+    }else if(waterTemp() <= 32){
+        frozenFooter.classList.remove('hide'); 
+        frozenFooter.classList.add('show');
+    }else{
+        goodTempFooter.classList.remove('hide'); 
+        goodTempFooter.classList.add('show');
+    }
+}, 3000);
+
